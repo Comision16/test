1755007e5c6617cc8ec63a8ccbd74882bdfc1d56
Comision16 <comision16formar@gmail.com>
2022-05-24 17:32:02 -0300

    agrego switch en app

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const calculadora = require('./modules/calculadora');
+const {sumar,restar} = require('./modules/calculadora');
 const {argv} = require('process');
 
 const operacion = argv[2];
+const n1 = argv[3];
+const n2 = argv[4];
+let resultado = 0;
 
-console.log(operacion);
+switch (operacion) {
+    case "sumar":
+        resultado = sumar(n1,n2)
+        break;
+
+    default:
+        break;
+}
+
+console.log(resultado);
